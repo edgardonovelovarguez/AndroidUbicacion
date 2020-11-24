5c1f4e3da8fd4aa91258dced0b653497cb5fb277
edgardo.novelo <edgardo.novelo@galletasdonde.com>
2020-11-24 16:25:21 -0600

    Agregar archivos de proyecto.

---
+++ AndroidUbicacion/AndroidUbicacion/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using AndroidUbicacion.ViewModels;
+
+namespace AndroidUbicacion.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
